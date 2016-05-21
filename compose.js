525981be262597ba0ce3a270d4e53acd158bdf5c
FCO <fernandocorrea@gmail.com>
2016-05-21 16:55:42 -0300

    Change classes

--- compose.js
+++ compose.js
@@ -1,29 +1,57 @@
+"use strict";
+
+let fsp = require('fs-promise');
+
+let Compose = {
+	base_dir		: "/tmp",
+	stacks			: {},
+	_get_or_new_stack	: function(name) {
+		if(name in this.stacks) {
+			return this.stacks[name]
+		}
+		return this.stacks[name] = new Compose.Stack(name);
+	},
+
+	save_stack		: function(name, content) {
+		let stack = this._get_or_new_stack(name);
+		return stack.save(content);
+	},
+
+	get_stack		: function(name) {
+		return this._get_or_new_stack(name);
+	}
+};
+
 module.exports = Compose;
 
-var fsp = require('fs-promise');
-
-function Compose() {
-	this.base_dir = "/tmp";
+Compose.Stack = function(name, content) {
+	if(name == undefined) throw "new stack with no name given";
+	this.name = name;
+	let promise = Promise.resolve();
+	if(content != undefined) {
+		promise = promise.then(data => this.save(content));
+	}
+	this.DONE = promise;
 }
 
-Compose.prototype = {
-	save_stack: function(name, content) {
-		var stack_dir = this.base_dir + "/" + name;
-		return fsp
+Compose.Stack.prototype = {
+	save	: function(content) {
+		 var stack_dir = Compose.base_dir + "/" + this.name;
+		 return fsp
 			.mkdir(stack_dir)
 			.catch(error => {
 				if (error.code == 'EEXIST') return null;
 				throw error;
-			})
-			.then(result =>
-				fsp.writeFile(stack_dir + "/docker-compose.yaml", content)
-			);
+		 	})
+			.then(result => fsp.writeFile(stack_dir + "/docker-compose.yaml", content))
+			.then(data => this)
+		;
 	},
-
-	get_stack: function(name) {
-		var stack_file = this.base_dir + "/" + name + "/docker-compose.yaml";
+	load	: function() {
+		var stack_file = Compose.base_dir + "/" + this.name + "/docker-compose.yaml";
 		return fsp
 			.readFile(stack_file, "utf-8")
-			.then(JSON.parse)
+			//.then(JSON.parse)
+		;
 	}
-}
+};
